edd49a348ab8c65795e6cd83a93c8282818150a6
thomas <thomas@vaadin.com>
2020-09-30 14:38:52 +0300

    Adds demo helper

--- src/main/java/com/vaadin/example/rest/ui/MainLayout.java
+++ src/main/java/com/vaadin/example/rest/ui/MainLayout.java
@@ -1,5 +1,6 @@
 package com.vaadin.example.rest.ui;
 
+import com.vaadin.example.corner.VaadinCorner;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -24,44 +25,46 @@ import com.vaadin.flow.router.RouterLink;
 @Push
 public class MainLayout extends AppLayout implements AfterNavigationObserver {
 
-	private final H1 pageTitle;
-	private final RouterLink home;
-	private final RouterLink inMemoryDTO;
-	private final RouterLink asyncInMemoryDTO;
-	private final RouterLink inMemoryJSON;
-	private final RouterLink lazyDTO;
+    private final H1 pageTitle;
+    private final RouterLink home;
+    private final RouterLink inMemoryDTO;
+    private final RouterLink asyncInMemoryDTO;
+    private final RouterLink inMemoryJSON;
+    private final RouterLink lazyDTO;
 
-	public MainLayout() {
-		// Navigation
-		home = new RouterLink("Home", HomeView.class);
-		inMemoryDTO = new RouterLink("In-Memory DTO", InMemoryDTOView.class);
-		asyncInMemoryDTO = new RouterLink("Asynchronous DTO", AsyncInMemoryDTOView.class);
-		inMemoryJSON = new RouterLink("In-Memory JSON", InMemoryJSONView.class);
-		lazyDTO = new RouterLink("Lazy DTO", LazyDTOView.class);
+    public MainLayout() {
+        // Navigation
+        home = new RouterLink("Home", HomeView.class);
+        inMemoryDTO = new RouterLink("In-Memory DTO", InMemoryDTOView.class);
+        asyncInMemoryDTO = new RouterLink("Asynchronous DTO", AsyncInMemoryDTOView.class);
+        inMemoryJSON = new RouterLink("In-Memory JSON", InMemoryJSONView.class);
+        lazyDTO = new RouterLink("Lazy DTO", LazyDTOView.class);
 
-		final UnorderedList list = new UnorderedList(new ListItem(home), new ListItem(inMemoryDTO),
-				new ListItem(asyncInMemoryDTO), new ListItem(inMemoryJSON), new ListItem(lazyDTO));
-		final Nav navigation = new Nav(list);
-		addToDrawer(navigation);
-		setPrimarySection(Section.DRAWER);
-		setDrawerOpened(false);
+        final UnorderedList list = new UnorderedList(new ListItem(home), new ListItem(inMemoryDTO), new ListItem(asyncInMemoryDTO), new ListItem(inMemoryJSON),
+                new ListItem(lazyDTO));
+        final Nav navigation = new Nav(list);
+        addToDrawer(navigation);
+        setPrimarySection(Section.DRAWER);
+        setDrawerOpened(false);
 
-		// Header
-		pageTitle = new H1("Home");
-		final Header header = new Header(new DrawerToggle(), pageTitle);
-		addToNavbar(header);
-	}
+        // Header
+        pageTitle = new H1("Home");
+        final Header header = new Header(new DrawerToggle(), pageTitle);
+        addToNavbar(header);
 
-	private RouterLink[] getRouterLinks() {
-		return new RouterLink[] { home, inMemoryDTO, inMemoryJSON, lazyDTO };
-	}
+        header.add(new VaadinCorner());
+    }
 
-	@Override
-	public void afterNavigation(AfterNavigationEvent event) {
-		for (final RouterLink routerLink : getRouterLinks()) {
-			if (routerLink.getHighlightCondition().shouldHighlight(routerLink, event)) {
-				pageTitle.setText(routerLink.getText());
-			}
-		}
-	}
+    private RouterLink[] getRouterLinks() {
+        return new RouterLink[] { home, inMemoryDTO, inMemoryJSON, lazyDTO };
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        for (final RouterLink routerLink : getRouterLinks()) {
+            if (routerLink.getHighlightCondition().shouldHighlight(routerLink, event)) {
+                pageTitle.setText(routerLink.getText());
+            }
+        }
+    }
 }
