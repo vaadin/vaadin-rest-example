53b16c3eefabab409b65e800c016429b7d9eb44e
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-07-28 15:57:05 +0300

    Update Vaadin to 20.0.5 (#52)

---
+++ src/main/java/com/vaadin/example/rest/AppShell.java
@@ -0,0 +1,8 @@
+package com.vaadin.example.rest;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+
+@Push
+public class AppShell implements AppShellConfigurator {
+}
